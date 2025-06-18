66b465a82dc7c1d70c601a1d62bb8da8b10972b6
Valentine Ncube <valentinemelly77@gmail.com>
2025-06-18 18:31:58 +1200

    added auction data

---
+++ auctionData.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    title: 'iphone 16 pro',
+    description: 'Latest model, slightly used, 128GB',
+    start_price: 1000,
+    reserve_price: 1200,
+  },
+
+  {
+    title: 'MacBook Pro 16',
+    description: '2020 model, M4 chip, 512GB SSD',
+    start_price: 2500,
+    reserve_price: 2900,
+  },
+
+  {
+    title: 'iPad Air 6th Gen',
+    description: 'Brand new, 11inch display, 256GB',
+    start_price: 1200,
+    reserve_price: 1400,
+  },
+]
