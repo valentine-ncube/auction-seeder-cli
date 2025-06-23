deb6c7a706718bec4a3fd98a3de07b710347808a
Valentine Ncube <valentinemelly77@gmail.com>
2025-06-23 17:36:23 +1200

    set up routes

--- API/server.js
+++ API/server.js
@@ -17,6 +17,25 @@ mongoose
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err))
 
+//Routes
+app.get('/api/search', async (req, res) => {
+  const query = req.query.query
+  if (!query)
+    return res.status(400).json({ error: 'Query parameter is required' })
+
+  try {
+    const results = await AuctionItem.find({
+      $or: [
+        { title: new RegExp(query, 'i') },
+        { description: new RegExp(query, 'i') },
+      ],
+    })
+    res.json(results)
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' })
+  }
+})
+
 //Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
